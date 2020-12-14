006da04cecf1eb614072771d473c39549516d5b9
sagar-savvy <sagarsk1302@gmail.com>
2020-12-14 15:41:46 +0530

    first push1

--- clientegy/views.py
+++ clientegy/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth import login,logout,authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from clientegy.models import *
-from django.contrib.auth.models import Permission
 from django.urls import reverse
 # Create your views here.
 def index(request):
